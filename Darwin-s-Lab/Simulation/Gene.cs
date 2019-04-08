a8db4b4e2cc34265ddf0d44197bba5e63035d566
Guillaume Chacun <guillaume.chacun@he-arc.ch>
2019-04-08 12:23:48 +0200

    Change gene's mutation probability

--- Darwin-s-Lab/Simulation/Gene.cs
+++ Darwin-s-Lab/Simulation/Gene.cs
@@ -11,7 +11,7 @@ namespace Darwin_s_Lab.Simulation
     /// </summary>
     class Gene
     {
-        public static double MutationProbability = 1.0;
+        public static double MutationProbability = 0.35;
 
         public String Name { get; set; }
         public uint Value { get; set; }
